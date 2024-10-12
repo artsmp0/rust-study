67c100f1e235e8fb7abffd22bc17c90dd4d89e5d
沈孟平 <artsmp@126.com>
2024-10-12 17:24:30 +0800

    primitive data types

---
+++ primitive-data-types-02/src/main.rs
@@ -0,0 +1,41 @@
+// int, float, char, bool
+
+// Integer types: u8, i8, u16, i16, u32, i32, u64, i64, u128, i128
+// Floating-point types: f32, f64
+// Boolean type: bool
+// Character type: char
+
+// Signed and unsigned integers
+// Signed integers: i8, i16, i32, i64, i128
+// Unsigned integers: u8, u16, u32, u64, u128
+// fn main() {
+//     let x: i8 = 127;
+//     println!("Signed integer x is {}", x);
+//     let y: u8 = 20;
+//     println!("Unsigned integer y is {}", y);
+// }
+
+// Maximum and minimum values
+// fn main() {
+//     println!("Maximum value of u8 is {}", u8::MAX); // 2^8 - 1 = 255
+//     println!("Minimum value of i8 is {}", i8::MIN); // -2^7 = -128, 2^7 - 1 = 127
+// }
+
+// Floating-point types
+// f32, f64
+// fn main() {
+//     let x: f64 = 3.14;
+//     println!("Floating-point number x is {}", x);
+// }
+
+// Boolean type
+// fn main() {
+//     let is_valid: bool = true;
+//     println!("Boolean value is {}", is_valid);
+// }
+
+// Character type
+fn main() {
+    let letter: char = 'A';
+    println!("The alphabet first letter is {}", letter);
+}
